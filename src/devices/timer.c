64ddef308e9d9665f6001fdce456d940da08f093
seunghwan-p <hseung0131@gmail.com>
2021-10-03 14:11:58 +0900

    Modify : time_sleep() in timer.c
    modify timer_sleep() function for alarm clock

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -90,10 +90,11 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks ();
-
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  thread_sleep(start + ticks);
+
+  /*while (timer_elapsed (start) < ticks) 
+    thread_yield ();*/
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
