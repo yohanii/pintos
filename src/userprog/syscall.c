9ffd83450cd75c5b95a0ae781e83d01bf916cf62
seunghwan-p <hseung0131@gmail.com>
2021-11-07 18:41:19 +0900

    add syscall exit write
    check pass argments passing
    fail from sc-bad-sp

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,89 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  switch (*(uint32_t *)(f->esp)) {
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
+      exit(*(uint32_t*)(f->esp+20));
+      break;
+    case SYS_EXEC:
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      write((int)*(uint32_t*)(f->esp+20), (void*)*(uint32_t*)(f->esp+24), (unsigned)*((uint32_t*)(f->esp+28)));
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
+
+  }   
+
+
+  //thread_exit ();
+}
+
+void exit(int status){
+  printf("%s: exit(%d)\n", thread_name(), status);
+  thread_exit();
+}
+
+int write(int fd, const void *buffer, unsigned size){
+  if(fd==1){
+    putbuf(buffer, size);
+    return size;
+  }
+  return -1;
+} 
+
+void halt(void){
+
+}
+
+pid_t exec(const char *cmd_lime){
+
+}
+int wait(pid_t pid){
+
+}
+bool create(const char *file, unsigned initial_size){
+
+}
+bool remove(const char *file){
+
+}
+int open(const char *file){
+
+}
+int filesize(int fd){
+
+}
+int read(int fd, void *buffer, unsigned size){
+
+}
+
+void seek(int fd, unsigned position){
+
+}
+unsigned tell(int fd){
+
+}
+void close(int fd){
+  
 }
