5847754d3ae02e742509f98a97d862c6ef4d1cd5
ubuntu <dygks924@gmail.com>
2021-11-09 08:28:18 -0800

    Solve exec-args

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -33,15 +33,17 @@ process_execute (const char *file_name)
   tid_t tid;
   char *saveptr;
   char *token;
+  char *save_fn;
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
+  save_fn = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
-
-  token = strtok_r(file_name, " ", &saveptr);
+  strlcpy (save_fn, file_name, PGSIZE);
+  token = strtok_r(save_fn, " ", &saveptr);
 
   if(filesys_open(token)==NULL){
     return -1;
@@ -51,7 +53,10 @@ process_execute (const char *file_name)
   tid = thread_create (token, PRI_DEFAULT, start_process, fn_copy);
   //printf("\n2222222222222\n");
   if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+    palloc_free_page (fn_copy);
+   
+  palloc_free_page(save_fn);
+  
   return tid;
 }
 
