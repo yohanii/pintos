38bd97822e9d8c146b8ce21dd12830f4a6c09c54
seunghwan-p <hseung0131@gmail.com>
2021-12-06 21:46:21 +0900

    implement lru policies

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -2,7 +2,15 @@
 
 struct list frame_table;
 struct list lru_list;
+struct list_elem *lru_clock;
 
+void
+lru_list_init (void)
+{
+  list_init (&lru_list);
+  list_init (&frame_table);
+  lru_clock = NULL;
+}
    
 static struct frame * get_frame (void) {
 /*palloc_get_page()에 PAL_USER을 넣어서 사용*/
@@ -13,7 +21,7 @@ frame 제거*/
 static struct frame *evict_frame (void) {
 /* for문을 이용해 frame_table을 돌면서 해당하는 frame 제거 */
 }
-bool claim_page (void *va ){//UNUSED) {
+bool claim_page (void *va ){
 /*user page가 이미 mapping 되었거나 메모리 할당 실패시 false return*/
 }
 
@@ -29,12 +37,40 @@ void __free_page(struct page *page)
 
 
 void add_page_to_lru_list(struct page *page){
-
+  list_push_back (&lru_list, &page->lru);
 }
 
 void del_page_from_lru_list(struct page *page){
-  
+
+  if (lru_clock == &page->lru)
+      lru_clock = list_remove (lru_clock);
+  else
+      list_remove (&page->lru);
 }
 struct list_elem* get_next_lru_clock(void){
-  
+
+   if(lru_clock == NULL){
+      if(!list_empty(&lru_list)){
+         lru_clock = list_begin (&lru_list);
+         return lru_clock;
+      }
+      else return NULL;
+   }
+   if(lru_clock == list_end(&lru_list)){
+      if(!list_empty(&lru_list)){
+         lru_clock = list_begin(&lru_list);
+         return lru_clock;
+      }
+      else return NULL;
+   }
+
+  lru_clock = list_next (lru_clock);
+  if (lru_clock == list_end (&lru_list)){
+		if(lru_clock == list_begin(&lru_list)){
+			return NULL;
+		}
+		else
+			lru_clock = list_begin(&lru_list);
+  }
+  return lru_clock;
 }
